bf77b030ca892edf4b427e0ca970a31ba2466b7a
Johannes Haseitl <johannes@undpaul.de>
2015-02-28 21:26:28 +0100

    Correct wording to "downstream"

--- subtree-split.sh
+++ subtree-split.sh
@@ -2,20 +2,21 @@
 
 # Environment variables needed
 #
-# - UPSTREAM_REPOSITORY
-# - UPSTREAM_BRANCH
+# - DOWNSTREAM_REPOSITORY
+# - DOWNSTREAM_BRANCH
 # - SUBTREE_PREFIX
 # - SUBTREE_BRANCH
 
 # Quit immediately if any command fails.
 set -e
 
-git remote add upstream $UPSTREAM_REPOSITORY
-git fetch upstream
+# Retrieve data from remote repository to build subtree from.
+git remote add downstream $DOWNSTREAM_REPOSITORY
+git fetch downstream
 
-git checkout -b "upstream" "upstream/$UPSTREAM_BRANCH"
+git checkout -b "downstream" "downstream/$DOWNSTREAM_BRANCH"
 
-# We split the subtree of the upstream in a new branch, so
+# We split the subtree of the downstream in a new branch, so
 git subtree split --prefix="$SUBTREE_PREFIX" --branch="$SUBTREE_BRANCH"
 
 #
